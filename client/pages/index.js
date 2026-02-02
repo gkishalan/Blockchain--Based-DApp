84e004a4f7384ed4dfe9b500c2c648eff857482c
gkishalan <princekishalan655@gmail.com>
2026-02-02 11:34:15 +0530

    Deleted the previous index.js code and created a new inde.js and provide connect metamask button if it click metamask will popup  and connect

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,33 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [account, setAccount] = useState("");
+
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert("Please install MetaMask");
+      return;
+    }
+
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    setAccount(accounts[0]);
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Blockchain Chat DApp</h1>
       <p>My first Web3 Chat Application</p>
+
+      {account ? (
+        <p>Connected Wallet: {account}</p>
+      ) : (
+        <button onClick={connectWallet}>
+          Connect Wallet
+        </button>
+      )}
     </div>
   );
 }
